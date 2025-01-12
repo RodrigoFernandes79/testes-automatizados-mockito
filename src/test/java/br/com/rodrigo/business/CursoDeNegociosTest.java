671e86a93ca05effbd3055844d4f65067a4494dc
Rodrigo Fernandes <rodrigohf79@hotmail.com>
2025-01-12 11:44:22 -0300

    setando valores a serem retornados pelo mock

--- src/test/java/br/com/rodrigo/business/CursoDeNegociosTest.java
+++ src/test/java/br/com/rodrigo/business/CursoDeNegociosTest.java
@@ -5,25 +5,45 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class CursoDeNegociosTest {
     CursoService cursoServiceMock;
     CursoDeNegocios cursoDeNegocios;
+    List<String> listaDeCursosSpringBootFiltrado;
 
     @BeforeEach
     void beforeEachMethod() {
         //Arrange /Given
         cursoServiceMock = mock(CursoService.class);
         cursoDeNegocios = new CursoDeNegocios(cursoServiceMock);
+        listaDeCursosSpringBootFiltrado = Arrays.asList(
+                "REST API's RESTFul do 0 à Azure com ASP.NET Core 5 e Docker",
+                "Agile Desmistificado com Scrum, XP, Kanban e Trello",
+                "Spotify Engineering Culture Desmistificado",
+                "REST API's RESTFul do 0 à AWS com Spring Boot 3 Java e Docker",
+                "Docker do Zero à Maestria - Contêinerização Desmistificada",
+                "Docker para Amazon AWS Implante Apps Java e .NET com Travis CI",
+                "Microsserviços do 0 com Spring Cloud, Spring Boot e Docker",
+                "Arquitetura de Microsserviços do 0 com ASP.NET, .NET 6 e C#",
+                "REST API's RESTFul do 0 à AWS com Spring Boot 3 Kotlin e Docker",
+                "Kotlin para DEV's Java: Aprenda a Linguagem Padrão do Android",
+                "Microsserviços do 0 com Spring Cloud, Kotlin e Docker"
+        );
     }
 
     @Test
     @DisplayName("TestandoBuscaDeCursosRelacionadosASpringBoot_Quando_UsandoUmMock")
     void buscandoTodosOsCursosDeSpringBootOndeOEstudanteEstaInscrito() {
+//Arrange / Given
+        given(cursoServiceMock.recuperarCursos("Rodrigo"))
+                .willReturn(listaDeCursosSpringBootFiltrado);
 
         //When / Act
         List<String> filtrandoTodosOsCursos = cursoDeNegocios
