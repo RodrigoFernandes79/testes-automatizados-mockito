9593ef6e783dc8d5f022182bd493bd7b4490f484
Rodrigo Fernandes <rodrigohf79@hotmail.com>
2025-01-12 10:34:35 -0300

    Entendendo o Conceito de System (Method) Under Test

---
+++ src/main/java/br/com/rodrigo/CursoDeNegocios.java
@@ -0,0 +1,32 @@
+package br.com.rodrigo;
+
+import br.com.rodrigo.service.CursoService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//System (method) Under Test - SUT
+public class CursoDeNegocios {
+
+    //CursoService é uma dependency
+    private CursoService cursoService;
+
+    public CursoDeNegocios(CursoService cursoService) {
+        this.cursoService = cursoService;
+    }
+
+    public List<String> buscandoTodosOsCursosDeSpringBootOndeOEstudanteEstaInscrito(String aluno) {
+
+        List<String> listaDeCursosSpringBootFiltrado = new ArrayList<>();
+        List<String> filtrandoTodosOsCursos = cursoService.recuperarCursos(aluno);
+
+        for (String cursoSpringBoot : filtrandoTodosOsCursos) {
+            if (cursoSpringBoot.contains("Spring")) {
+                listaDeCursosSpringBootFiltrado.add(cursoSpringBoot);
+            }
+        }
+        return listaDeCursosSpringBootFiltrado;
+    }
+
+
+}
---
+++ src/main/java/br/com/rodrigo/service/CursoService.java
@@ -0,0 +1,8 @@
+package br.com.rodrigo.service;
+
+import java.util.List;
+
+public interface CursoService {
+
+    List<String> recuperarCursos(String aluno);
+}
